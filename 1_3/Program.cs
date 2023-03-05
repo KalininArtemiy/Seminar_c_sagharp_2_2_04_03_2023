5561042fdc99017524ce1a3d72618bf3b9702216
Kalinin <9776020@mail.ru>
2023-03-05 19:09:46 +0300

    Task1_3

---
+++ 1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿void pow_numbers(int number)
+{
+  int index = 1;
+  while(index <= number)
+  {
+    Console.Write(Math.Pow(index, 2) + " ");
+    index = index + 1; 
+  }
+}
+pow_numbers(int.Parse(Console.ReadLine()));
