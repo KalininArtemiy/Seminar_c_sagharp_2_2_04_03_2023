7187c5795fb61607d7e31e9da989a01e97253780
Kalinin <9776020@mail.ru>
2023-03-05 19:26:08 +0300

    HW Task1_1

---
+++ Homework/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿void is_palindrom(string number)
+{
+  int len = number.Length;
+  if (len == 5)
+  {
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+      Console.WriteLine("Is Palindrom");
+    }
+    else  Console.WriteLine("Not Palindrom");
+  }
+  else Console.WriteLine("Has no five symbols");
+}
+is_palindrom(Console.ReadLine());
